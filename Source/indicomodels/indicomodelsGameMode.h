cda3ea739c13a855274bea8160f654f1f15e6ed1
Ovodus <ovodus@yandex.ru>
2016-11-01 08:19:20 +0300

    Initial Commit

---
+++ Source/indicomodels/indicomodelsGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "indicomodelsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INDICOMODELS_API AindicomodelsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
